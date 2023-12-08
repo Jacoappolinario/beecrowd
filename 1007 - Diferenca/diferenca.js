56d3e4bbbc384fc72fc25fe3c4d54b1256ba2df8
Jacó Apolinário <53705089+Jacoappolinario@users.noreply.github.com>
2023-12-08 17:32:46 -0300

    add exercise 1007

---
+++ 1007 - Diferenca/diferenca.js
@@ -0,0 +1,32 @@
+const { readFile } = require("fs").promises;
+
+const INPUT_FILE = process.argv[2] || "/dev/stdin";
+
+async function main() {
+  try {
+    const inputContent = (await readFile(INPUT_FILE, "utf-8"))
+      .trim()
+      .split("\n")
+      .map((value) => parseInt(value));
+
+    if (inputContent.some(isNaN)) {
+      throw new Error(
+        `Error: Invalid input value. Please check the values assigned to A, B, C and D`
+      );
+    }
+
+    const [A, B, C, D] = inputContent;
+
+    const difference = A * B - C * D;
+
+    console.log(`DIFERENCA = ${difference}`);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.error(`Error: The file ${INPUT_FILE} could not be found.`);
+    } else {
+      console.error(`Error processing file ${INPUT_FILE}: ${error.message}`);
+    }
+  }
+}
+
+main();
